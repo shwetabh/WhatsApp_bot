fbaa9ebcbac4f93979352f6f5bf3290dfa8d2a48
Nikhil Sharma <nikhil.21it447@rtu.ac.in>
2023-04-12 07:59:09 +0530

    adding some more features along with functionalisation

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
-const {Client} = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
-const client = new Client();
+//for badword filteration
+const badwordsArray = require("badwords/array");
 
-client.on('qr',(qr) =>{
-    qrcode.generate(qr,{small:true});
+// const client = new Client();
+const client = new Client({
+  authStrategy: new LocalAuth(),
 });
 
-client.on('ready',() =>{
-    console.log("Client is ready");
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("You are ready to go");
 });
 
 client.initialize();
 
 const configuration = new Configuration({
-    apiKey : process.env.SECRET_KEY,
+  apiKey: process.env.SECRET_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function runCompletion(message){
-    const completion = await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt: message,
-        max_tokens: 200,
-    });
-    return completion.data.choices[0].text;
+async function runCompletion(message) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message,
+    max_tokens: 1000,
+  });
+  return completion.data.choices[0].text;
 }
-
-client.on('message',message => {
-    console.log(message.body);
-    runCompletion(message.body).then(result => message.reply(result));
-})
+const check = (reply) => {
+  //Cheking for extra spaces using trim
+  ((reply) => {
+    if (reply.trim() === "" && reply.trim() === " ") {
+      return false;
+    }
+  })(reply);
+  //Checking if message contains any bad words:
+  ((reply) => {
+    reply = String(reply);
+    badwordsArray.forEach((badWord, i, arr) => {
+      if (reply.includes(badWord)) {
+        console.log(`BadWord = ${badWord}, Reply = ${reply}`);
+        return false;
+      } else return true;
+    });
+  })(reply);
+  //reply.length>400 (not sent)
+  ((reply) => {
+    console.log("length = ", reply.trim().length);
+    reply.trim().length > 400 ? false : true;
+  })(reply);
+};
+client.on("message", (message) => {
+  runCompletion(message.body).then((reply) => {
+    //Checking for all kind of mistakes before reply
+    if (check(reply)) {
+      console.log("EMPTY orHas BADWORDS");
+      console.log(reply.trim() + "____its the reply and so not sent");
+    } else {
+      message.reply(`${reply.trim()}\nAutomated reply from ChatBot`);
+      console.log("SENT= ", reply.trim());
+    }
+  });
+});
