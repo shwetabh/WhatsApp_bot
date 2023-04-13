1ceb99d90282374ff787d7c0605b4dcdf833378b
Nikhil Sharma <nikhil.21it447@rtu.ac.in>
2023-04-14 04:06:28 +0530

    Improved functionality fo code and logic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const { Configuration, OpenAIApi } = require("openai");
@@ -26,6 +28,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+/* Function sending messages to chatGPT and returning repy */
 async function runCompletion(message) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
@@ -34,38 +37,75 @@ async function runCompletion(message) {
   });
   return completion.data.choices[0].text;
 }
+/* Function Checking for all mistakes in reply */
 const check = (reply) => {
-  //Cheking for extra spaces using trim
-  ((reply) => {
-    if (reply.trim() === "" && reply.trim() === " ") {
-      return false;
-    }
-  })(reply);
-  //Checking if message contains any bad words:
-  ((reply) => {
-    reply = String(reply);
-    badwordsArray.forEach((badWord, i, arr) => {
-      if (reply.includes(badWord)) {
-        console.log(`BadWord = ${badWord}, Reply = ${reply}`);
+  const performCheck = {
+    //Checking reply for bad words
+    badWordCheck: (reply) => {
+      reply = String(reply);
+      const trialWork = [];
+      badwordsArray.forEach((badWord, i, arr) => {
+        if (reply.includes(badWord)) {
+          console.log(`BadWord = ${badWord}, Reply = ${reply}`);
+          trialWork.push(false);
+        } else {
+          trialWork.push(true);
+        }
+      });
+      const badWordCheckResult = trialWork.reduce(
+        (accumulator, booleanValue) => accumulator && booleanValue,
+        true
+      );
+      console.log(`badWordCheckResult = ${badWordCheckResult}`);
+      return badWordCheckResult;
+    },
+
+    //Checking reply length
+    lengthCheck: (reply) => {
+      console.log("length = ", reply.trim().length);
+      return reply.trim().length > 400 ? false : true;
+    },
+
+    //Checking for empty reply
+    emptyReplyCheck: (reply) => {
+      if (reply.trim().length === 0) {
+        console.log("Length = 0", reply.trim().length);
         return false;
       } else return true;
-    });
-  })(reply);
-  //reply.length>400 (not sent)
-  ((reply) => {
-    console.log("length = ", reply.trim().length);
-    reply.trim().length > 400 ? false : true;
-  })(reply);
+    },
+  };
+
+  console.log(
+    "my reply",
+    performCheck.badWordCheck(reply) &&
+      performCheck.lengthCheck(reply) &&
+      performCheck.emptyReplyCheck(reply)
+  );
+  return (
+    performCheck.badWordCheck(reply) &&
+    performCheck.lengthCheck(reply) &&
+    performCheck.emptyReplyCheck(reply)
+  );
 };
+
 client.on("message", (message) => {
   runCompletion(message.body).then((reply) => {
     //Checking for all kind of mistakes before reply
     if (check(reply)) {
-      console.log("EMPTY orHas BADWORDS");
-      console.log(reply.trim() + "____its the reply and so not sent");
+      console.log(reply.trim() + "__reply from true");
+      console.log("----------------------------");
+    } else if (!check(reply)) {
+      console.log("it is false");
+      console.log("----------------------------");
     } else {
-      message.reply(`${reply.trim()}\nAutomated reply from ChatBot`);
-      console.log("SENT= ", reply.trim());
+      console.log(
+        "Their might be some bugs as the check(reply) may be returned a undefined"
+      );
+      console.log("Reply = ", reply.trim());
+      console.log("----------------------------");
     }
   });
 });
+
+// Bugs that needs to be fixed
+/* 1. reply containing classes won't be send as it is being takes as a bad word because classes includes 'ass' */
